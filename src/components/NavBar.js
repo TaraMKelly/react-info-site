fd064939a107f6d15bc5557700c160db742f426d
TaraMKelly <tara.kelly16@gmail.com>
2022-03-07 16:01:00 -0500

    more styling basics

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav> 
+          {/* <img src='../images/logo192.png' alt="logo"/> */}
+          <h3>React Facts</h3>
+          <h4>React Course - Project 1</h4>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
